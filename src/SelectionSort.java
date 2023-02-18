1d490e7b5e48dca5d5318d185fe136f9ce3f312c
SlavikKochiyan <slava.user.97@ukr.net>
2023-02-18 18:52:37 +0200

    Add new type of sorting

---
+++ src/SelectionSort.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class SelectionSort {
+    static Scanner inputData;
+    int size;
+    int [] numbers;
+    static Random rn;
+
+    public static void main(String[] args) {
+
+    }
+}
